73f903b2e21d40b79c97f6f655944bf92b69f38f
Michał Doroz <michaldoroz7@gmail.com>
2021-07-01 19:24:26 +0200

    Edited previous commit - Player Class with pojo

--- src/Person/Player.java
+++ src/Person/Player.java
@@ -1,2 +1,31 @@
-package Person;public class Player {
+package Person;
+
+public class Player {
+    String firstName;
+    String lastName;
+    int budget;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
 }
