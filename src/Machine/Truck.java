0f13a9bbecc76cba07273c77076b556b868e8fe9
Michał Doroz <michaldoroz7@gmail.com>
2021-07-01 20:33:00 +0200

    Added Truck class, #12

---
+++ src/Machine/Truck.java
@@ -0,0 +1,9 @@
+package Machine;
+
+public class Truck extends Vehicle{
+
+    public Truck(int price, String producent, double mileage, String color, String segment, String suspension, String breaks, String engine, String body, String gearbox){
+        super(price, producent, mileage, color, segment, suspension, breaks, engine, body, gearbox);
+    }
+
+}
